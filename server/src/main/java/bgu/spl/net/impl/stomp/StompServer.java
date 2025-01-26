6ab63ee8f67e60b377c236b0cad25cc066bf8f8a
RanIvgi <ranivgi1@gmail.com>
2025-01-26 16:09:48 +0000

    ServerWithArgs&MakeFileBeforeSubmision

--- server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -5,20 +5,34 @@ import bgu.spl.net.srv.Server;
 public class StompServer {
 
     public static void main(String[] args) {
-        // TODO: implement this
 
-        // you can use any server...
-        Server.threadPerClient(
-                7777, // port
-                () -> new StompMessagingProtocolImp(), // protocol factory
-                StompMessageEncoderDecoder::new // message encoder decoder factory
-        ).serve();
+        // Not right args
+        if (args.length == 0) {
+            System.out.println("Usage: StompServer <port> <TypeServer>");
+            System.exit(1);
+        }
 
-        // Server.reactor(
-        //         Runtime.getRuntime().availableProcessors(),
-        //         7777, // port
-        //         () -> new StompMessagingProtocolImp(), // protocol factory
-        //         StompMessageEncoderDecoder::new // message encoder decoder factory
-        // ).serve();
+        
+        // start the server with the thread-per-client server
+        if (args[1].equals("tpc")) {
+            Server.threadPerClient(
+                    Integer.parseInt(args[0]), // port
+                    () -> new StompMessagingProtocolImp(), // protocol factory
+                    StompMessageEncoderDecoder::new // message encoder decoder factory
+            ).serve();
+        
+        }
+        // Start the server with the reactor 
+        else if (args[1].equals("reactor")) {
+            Server.reactor(
+                    Runtime.getRuntime().availableProcessors(),
+                    Integer.parseInt(args[0]), // port
+                    () -> new StompMessagingProtocolImp(), // protocol factory
+                    StompMessageEncoderDecoder::new // message encoder decoder factory
+            ).serve();
+        } else {
+            System.out.println("Usage: StompServer <port> <TypeServer>");
+            System.exit(1);
+        }
     }
 }
